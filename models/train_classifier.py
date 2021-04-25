5153ea55d418ce87d98c22d3aefb8a0513349630
Ola Kozera <kozersky@MacBook-Pro.local>
2021-04-25 13:52:50 +0200

    Add models section

---
+++ models/train_classifier.py
@@ -0,0 +1,111 @@
+# import libraries
+import sys
+import pandas as pd
+from sqlalchemy import create_engine
+import re
+
+import nltk
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import stopwords
+
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.datasets import make_multilabel_classification
+from sklearn.multioutput import MultiOutputClassifier
+
+
+def load_data(database_filepath):
+    # load data from database
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql("SELECT * FROM DisasterResponse", engine)
+    
+    category_names = ['related', 'request', 'offer',
+       'aid_related', 'medical_help', 'medical_products', 'search_and_rescue',
+       'security', 'military', 'child_alone', 'water', 'food', 'shelter',
+       'clothing', 'money', 'missing_people', 'refugees', 'death', 'other_aid',
+       'infrastructure_related', 'transport', 'buildings', 'electricity',
+       'tools', 'hospitals', 'shops', 'aid_centers', 'other_infrastructure',
+       'weather_related', 'floods', 'storm', 'fire', 'earthquake', 'cold',
+       'other_weather', 'direct_report']
+
+    X = df['message'].values
+    Y = df[category_names].values
+    
+    return X, Y, category_names
+
+def tokenize(text):
+
+    # clean text - remove special characters, normalize case 
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text).lower()
+    
+    # tokenize text
+    tokens = nltk.word_tokenize(text)
+    
+    # initiate lemmatizer
+    lemmatizer = WordNetLemmatizer()
+
+    # lemmatize each token
+    clean_tokens = [lemmatizer.lemmatize(tok) for tok in tokens]
+    
+    return clean_tokens
+
+
+def build_model():
+
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer = tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier())),
+    ])
+
+    return pipeline
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    
+    from sklearn.metrics import classification_report
+
+    Y_pred = model.predict(X_test)
+    
+    for i, cat in enumerate(category_names):
+        print('Classification report for {}: \n {}'.format(cat, classification_report(Y_test[:,i], Y_pred[:,i])))
+
+
+def save_model(model, model_filepath):
+
+    import pickle
+    pickle.dump(model, open(model_filepath, 'wb'))
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
